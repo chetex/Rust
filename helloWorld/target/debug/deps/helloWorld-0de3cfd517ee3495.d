e9236d941e2e6a922a4e80d33bb3537bdef8a530
chetex <chetex69@gmail.com>
2025-03-16 21:50:37 +0100

    init instalation

---
+++ helloWorld/target/debug/deps/helloWorld-0de3cfd517ee3495.d
@@ -0,0 +1,5 @@
+/Users/ignaciogarciagomez/Desarrollo/Rust/helloWorld/target/debug/deps/libhelloWorld-0de3cfd517ee3495.rmeta: src/main.rs
+
+/Users/ignaciogarciagomez/Desarrollo/Rust/helloWorld/target/debug/deps/helloWorld-0de3cfd517ee3495.d: src/main.rs
+
+src/main.rs:
